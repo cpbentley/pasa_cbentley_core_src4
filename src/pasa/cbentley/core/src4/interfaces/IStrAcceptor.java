16edf5e6b5fee90a3123ccf1938885b5d568367b
Charles Bentley <cbentley@mail.ru>
2019-07-16 23:20:37 +0200

    comments

--- src/pasa/cbentley/core/src4/interfaces/IStrAcceptor.java
+++ src/pasa/cbentley/core/src4/interfaces/IStrAcceptor.java
@@ -1,7 +1,17 @@
 package pasa.cbentley.core.src4.interfaces;
 
-public interface IStrAcceptor {
+import pasa.cbentley.core.src4.logging.IStringable;
 
-   
+/**
+ * 
+ * @author Charles Bentley
+ *
+ */
+public interface IStrAcceptor extends IStringable {
+   /**
+    * true if String is accepted
+    * @param str
+    * @return
+    */
    public boolean isStringAccepted(String str);
 }
