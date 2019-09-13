dea34345bdeb86ea43b35888d5759657946ef492
Charles Bentley <cbentley@mail.ru>
2019-09-13 16:24:46 +0200

    getNumericalValue

--- src/pasa/cbentley/core/src4/utils/CharUtils.java
+++ src/pasa/cbentley/core/src4/utils/CharUtils.java
@@ -103,6 +103,37 @@ public class CharUtils {
    }
 
    /**
+    * Returns 0-9 values exclusively from plane 0
+    * @param c
+    * @return -1 if it does not have a numerical value
+    */
+   public int getNumericalValue(char c) {
+      switch (c) {
+         case '0':
+            return 0;
+         case '1':
+            return 1;
+         case '2':
+            return 2;
+         case '3':
+            return 3;
+         case '4':
+            return 4;
+         case '5':
+            return 5;
+         case '6':
+            return 6;
+         case '7':
+            return 7;
+         case '8':
+            return 8;
+         case '9':
+            return 9;
+         default:
+            return -1;
+      }
+   }
+   /**
     * Assume v is 0 based for lowercase letter a
     * @param v
     * @param plane
